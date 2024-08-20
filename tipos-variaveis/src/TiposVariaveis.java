5fab74bc23bbb5b38810e3c669a028c85208f340
Joaodev <devjoa1v@gmail.com>
2024-08-20 20:57:20 -0300

    renomeando a pasta java e escrevendo anotações

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,24 @@
+public class TiposVariaveis {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //estudem a classe string que representa texto na aplicação
+
+        String meuNome = "Joao";
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        
+        numero = 10;
+
+        System.out.println("Hello, World!");
+    }
+}
