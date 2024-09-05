da95c70331fbc337609e03cf25da0ce989741404
Joaodev <devjoa1v@gmail.com>
2024-09-05 19:49:14 -0300

    aula de try, catch

---
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -0,0 +1,20 @@
+public class FormatadorCepExemplo {
+    
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("6550000");
+            System.out.println(cepFormatado);
+        
+        } catch (CepInvalidoException e) {
+            System.out.println("o cep não correspomde com as regras de negocio");
+
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+            throw new CepInvalidoException();
+
+            return "23.765-064";
+
+    }
+}
